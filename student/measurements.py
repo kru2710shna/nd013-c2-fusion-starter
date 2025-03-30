669e2c42b017892b1e1eb53157b0dc140f8a07ca
Krushna Thakkar <109793947+kru2710shna@users.noreply.github.com>
2025-03-30 15:44:47 -0700

    WriteUp

--- student/measurements.py
+++ student/measurements.py
@@ -138,9 +138,8 @@ class Sensor:
         # Step 4: remove restriction to lidar in order to include camera as well
         ############
         
-        if self.name == 'lidar':
-            meas = Measurement(num_frame, z, self)
-            meas_list.append(meas)
+        meas = Measurement(num_frame, z, self)
+        meas_list.append(meas)
         return meas_list
         
         ############
